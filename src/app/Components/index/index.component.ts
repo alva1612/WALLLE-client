96ac6f7d575cb4fa810c46ccf039615da5646ead
adriansotoh <adriansotohidalgo16@gmail.com>
2022-10-27 16:25:09 -0500

    rename components and validate user token

--- src/app/Components/index/index.component.ts
+++ src/app/Components/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/security/token.service';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  isLogged = false;
+  nombreUsuario = "";
+
+  constructor(private tokenService:TokenService, private _router:Router) { }
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+      this.nombreUsuario = this.tokenService.getUserNameComplete()|| '{}';
+    } else {
+      this.isLogged = false;
+      this.nombreUsuario = '';
+      this._router.navigate(['/login'])
+    }
   }
 
 }
