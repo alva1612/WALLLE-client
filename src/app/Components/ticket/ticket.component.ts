66682ba1768e4a31330eb6de963932ac648f565b
Andres <andresgallegosluque@gmail.com>
2022-11-08 18:52:11 -0500

    varios cambios

--- src/app/Components/ticket/ticket.component.ts
+++ src/app/Components/ticket/ticket.component.ts
@@ -3,6 +3,7 @@ import { Dificultad, Estado, Ticket, Trabajador, Urgencia } from 'src/app/Models
 import { TicketService } from 'src/app/services/ticket.service';
 import { UtilService } from 'src/app/services/util.service';
 import Swal from 'sweetalert2'
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket',
@@ -31,7 +32,16 @@ export class TicketComponent implements OnInit {
       id_dificultad: 1
     } as Dificultad,
     id_ticket: 0
-  }
+  };
+
+  //   //para verificar que e pulsó el boton
+  //   submitted = false;
+
+  //   formsRegistra = new FormGroup({
+  //     validaTitulo: new FormControl('', [Validators.required, Validators.pattern('[a-zA-ZáéíóúÁÉÍÓÚñ0-9 ]{3,30}')]),
+ 
+  // });
+  
 
   constructor(private ticketService: TicketService, private utilService: UtilService) {
 
