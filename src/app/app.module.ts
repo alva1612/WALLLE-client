fb7435822c11a13abb0f1fffdf3efb7bff69119c
Alvaro <alvaro.guillenc1612@gmail.com>
2022-11-13 21:33:31 -0500

    reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { ProdInterceptorService } from './interceptors/prod-interceptor.service'
 import { DetalleTicketComponent } from './Components/ticket/detalle-ticket/detalle-ticket.component';
 import { AgregarTicketComponent } from './Components/ticket/agregar-ticket/agregar-ticket.component';
 import { EncargadoModule } from './Components/encargado/encargado.module';
-import { ConsultaTickerEstadoComponent } from './Components/consulta-ticker-estado/consulta-ticker-estado.component';
+import { ConsultaTickerEstadoComponent } from './components/consulta-ticker-estado/consulta-ticker-estado.component';
 import { ListadoTicketComponent } from './Components/listado-tickets/listado-ticket.component';
 
 @NgModule({
