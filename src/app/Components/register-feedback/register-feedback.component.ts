d835404a01f3ec60075b58ec45e6ef80aee1fd8b
Alvaro <alvaro.guillenc1612@gmail.com>
2022-11-17 02:02:22 -0500

    ticket auto id

--- src/app/Components/register-feedback/register-feedback.component.ts
+++ src/app/Components/register-feedback/register-feedback.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { tick } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Feedback } from 'src/app/Models/Models';
 import { TicketService } from 'src/app/services/ticket.service';
 import Swal from 'sweetalert2';
@@ -12,12 +13,25 @@ import Swal from 'sweetalert2';
 export class RegisterFeedbackComponent implements OnInit {
 
   feedback: Feedback = {
+    id_ticket: 0,
     estrellas: 0
   } as Feedback
 
-  constructor(private _ticketService: TicketService) { }
+  constructor(
+    private _ticketService: TicketService,
+    private _router: Router,
+    private _route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this._route.queryParams
+      .subscribe(params => {
+        console.log(params)
+        this.feedback.id_ticket = params['id'];
+
+        const ticket: HTMLInputElement = document.getElementById('ticket')! as HTMLInputElement
+        ticket.value = this.feedback.id_ticket.toString()
+      })
   }
 
   onChangeStar(number:number) {
@@ -28,9 +42,10 @@ export class RegisterFeedbackComponent implements OnInit {
     console.log('envio')
     const ticket: HTMLInputElement = document.getElementById('ticket')! as HTMLInputElement
     const idTicket = Number(ticket.value)
+    this.feedback.id_ticket = idTicket
     const opinion = document.getElementById('opinion')! as HTMLInputElement
     const opinionValue = opinion.value
-    this._ticketService.registrarFeedback(idTicket, this.feedback.estrellas, opinionValue).subscribe(res => {
+    this._ticketService.registrarFeedback(this.feedback.id_ticket, this.feedback.estrellas, opinionValue).subscribe(res => {
       console.log(res)
       Swal.fire(res.mensaje)
     })
