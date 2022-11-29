750c107ab25c4768494e603b9974fcab392ad63f
adriansotoh <adriansotohidalgo16@gmail.com>
2022-11-29 17:56:04 -0500

    correciones en actualizar e implementando soap

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,5 +2,5 @@ export class AppSettings {
 
     public static API_ENDPOINT='http://localhost:8090';
 
-    public static API_ENDPOINT_MICRO='http://localhost:8094';
+    public static API_ENDPOINT_MICRO='http://localhost:8092';
  }
